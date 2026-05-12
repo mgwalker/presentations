ae6bd3b66edefa594eefb8f5c5d971597b879d25
Greg Walker <git@greg.email>
2026-05-12 13:54:23 -0500

    intro to apis, part 1

--- src/assets/reveal.js
+++ src/assets/reveal.js
@@ -1,5 +1,6 @@
 import reveal from "reveal.js";
 import highlight from "reveal.js/plugin/highlight";
+import markdown from "reveal.js/plugin/markdown";
 import notes from "reveal.js/plugin/notes";
 
 reveal.initialize({
@@ -7,7 +8,7 @@ reveal.initialize({
   disableLayout: true,
   hash: true,
   hashOneBasedIndex: true,
-  plugins: [highlight, notes],
+  plugins: [highlight, markdown, notes],
   progress: true,
   transition: "none",
 });
